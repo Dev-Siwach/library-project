a0159cbe57202b2a40d5173114d207ba4b8d5459
Dev Siwach <Dev652007@proton.me>
2024-07-06 18:13:26 +0530

    restart

--- script.js
+++ script.js
@@ -1,44 +1,8 @@
-const myLibrary = [{ name: "ABC", author: "abc", pages: 123, read: true }];
+/* declare variable for dom elements */
+const addBookDialog = document.getElementById("addBookDialog");
+const openDialogBoxButton = document.getElementById("openDialogBoxButton");
 
-/*
-document
-  .getElementById("addBookButton")
-  .addEventListener("click", addBookToLibrary);
-  */
-
-/*
-function addBookToLibrary() {
-  var titleInput = prompt("What is the name of the book? ");
-  var authorInput = prompt("Who is the author? ");
-  var pagesInput = prompt("How many pages does it have? ");
-  var readInput = prompt("Have you read it?");
-  Book(titleInput, authorInput, pagesInput, readInput);
-  console.table(myLibrary);
-  var bookCard = document.createElement("div");
-  bookCard.textContent =
-    myLibrary[myLibrary.length - 1].name +
-    " by " +
-    myLibrary[myLibrary.length - 1].author +
-    " have " +
-    myLibrary[myLibrary.length - 1].pages +
-    " pages .";
-  var bookList = document.getElementById("bookList");
-  bookList.appendChild(bookCard);
-}
-*/
-
-/*
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.array = {
-    name: this.title,
-    author: this.author,
-    pages: this.pages,
-    read: this.read,
-  };
-  myLibrary.push(this.array);
-}
-*/
+/* opens dialog box when clicking on add box */
+openDialogBoxButton.addEventListener("click", () => {
+  addBookDialog.showModal();
+});
